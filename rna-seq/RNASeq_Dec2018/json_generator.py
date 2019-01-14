fc44df2d72ecaa3571ffb81c07afd303f5d95e4b
Nathaniel <njmaki@mtu.edu>
2019-01-14 17:09:56 -0500

    Update json_generator.py

--- rna-seq/RNASeq_Dec2018/json_generator.py
+++ rna-seq/RNASeq_Dec2018/json_generator.py
@@ -1,8 +1,21 @@
 import json
+import sys
+import glob
+import errno
 import os
 
-# lists all files with .json filetype in given directory
-for file  in os.listdir("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018"):
-    if file.endswith(".json"):
-        print(os.path.join("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018", file))
+# # lists all files with .json filetype in given directory
+# for file  in os.listdir("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018"):
+#     if file.endswith(".json"):
+#         print(os.path.join("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018", file))
 
+path = '/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018/*.json'
+files = glob.glob(path)
+
+for name in files: 
+    try:
+        with open(name) as f:
+            sys.stdout.write(f.read())
+    except IOError as exc:
+        if exc.errno != errno.EISDIR:
+            raise
