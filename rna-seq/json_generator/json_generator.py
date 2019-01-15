fdc87f18164e09d1eb85fd37f75b79d01d4b9a05
Nathaniel <njmaki@mtu.edu>
2019-01-15 12:00:01 -0500

    Update json_generator

--- rna-seq/json_generator/json_generator.py
+++ rna-seq/json_generator/json_generator.py
@@ -4,10 +4,10 @@ import glob
 import errno
 import os
 
-# # lists all files with .json filetype in given directory
-# for file  in os.listdir("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018"):
-#     if file.endswith(".json"):
-#         print(os.path.join("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018", file))
+# lists all files with .json filetype in given directory
+for file  in os.listdir("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018"):
+    if file.endswith(".json"):
+        print(os.path.join("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018", file))
 
 path = '/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018/*.json'
 files = glob.glob(path)
@@ -20,4 +20,14 @@ for name in files:
     except IOError as exc:
         if exc.errno != errno.EISDIR:
             raise
-            
+
+read_path = '/data/scratch/rna-seq/RNASeq_Dec2018/*.fastq'
+read_files = glob.glob(read_path)
+
+for name in read_files:
+    try:
+        with open(name) as rf:
+            sys.stdout.write(rf.read())
+    except IOError as exc:
+        if exc.errno != errno.EISDIR:
+            raise
