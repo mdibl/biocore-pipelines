7ef3448fa78e029f9dbddcece292856ad4a3026c
Nathaniel <njmaki@mtu.edu>
2019-01-15 16:58:12 -0500

    Add array load

--- rna-seq/json_generator/generator.py
+++ rna-seq/json_generator/generator.py
@@ -3,6 +3,7 @@ import sys
 import glob
 import errno
 import os
+import pprint
 
 # lists all files with .fastq filetype in given directory
 for file  in os.listdir('/data/scratch/rna-seq/RNASeq_Dec2018/'):
@@ -25,6 +26,14 @@ template_array = []
 with open('template.json') as my_file:
         for line in my_file:
                 template_array.append(line)
+
+from pprint import pprint
+
+with open('template.json') as f:
+        template = json.load(f)
+
+pprint(template)
+
 
 # read_path = '/data/scratch/rna-seq/RNASeq_Dec2018/*.fastq'
 # read_files = glob.glob(read_path)
