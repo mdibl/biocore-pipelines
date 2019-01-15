218edcbd7a746a8ee418b10426db1b89d6231a85
Nathaniel <njmaki@mtu.edu>
2019-01-15 14:20:55 -0500

    Update json_generator

--- rna-seq/json_generator/json_generator.py
+++ rna-seq/json_generator/json_generator.py
@@ -4,15 +4,15 @@ import glob
 import errno
 import os
 
-# lists all files with .json filetype in given directory
-for file  in os.listdir("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018"):
-    if file.endswith(".json"):
-        print(os.path.join("/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018", file))
+# lists all files with .fastq filetype in given directory
+for file  in os.listdir('/data/scratch/rna-seq/RNASeq_Dec2018/'):
+    if file.endswith(".fastq"):
+        print(os.path.join("/data/scratch/rna-seq/RNASeq_Dec2018", file))
 
-path = '/Users/nmaki/Documents/GitHub/biocore-pipelines/rna-seq/RNASeq_Dec2018/*.json'
+path = '/data/projects/Biocore/biocore-pipelines/rna-seq/json_generator/*.json'
 files = glob.glob(path)
 
-# loads json files into an array and displays content
+# loads json template and displays content
 for name in files: 
     try:
         with open(name) as f:
@@ -21,13 +21,18 @@ for name in files:
         if exc.errno != errno.EISDIR:
             raise
 
-read_path = '/data/scratch/rna-seq/RNASeq_Dec2018/*.fastq'
-read_files = glob.glob(read_path)
+json_array = []
+with open('template.json') as json_file:
+        for line in json_file:
+                json_array.append(line)
 
-for name in read_files:
-    try:
-        with open(name) as rf:
-            sys.stdout.write(rf.read())
-    except IOError as exc:
-        if exc.errno != errno.EISDIR:
-            raise
+# read_path = '/data/scratch/rna-seq/RNASeq_Dec2018/*.fastq'
+# read_files = glob.glob(read_path)
+
+# for name in read_files:
+#     try:
+#         with open(name) as f:
+#             sys.stdout.write(f.read())
+#     except IOError as exc:
+#         if exc.errno != errno.EISDIR:
+#             raise
