d7ad6a77c391f69cebbfac83ffa92e0c194fc09f
Nathaniel <njmaki@mtu.edu>
2019-01-16 13:45:26 -0500

    Update generator.py

--- rna-seq/json_generator/generator.py
+++ rna-seq/json_generator/generator.py
@@ -13,7 +13,7 @@ for file  in os.listdir('/data/scratch/rna-seq/RNASeq_Dec2018/'):
 path = '/data/projects/Biocore/biocore-pipelines/rna-seq/json_generator/*.json'
 files = glob.glob(path)
 
-# loads json files into an array and displays content
+
 for name in files: 
     try:
         with open(name) as f:
@@ -22,6 +22,7 @@ for name in files:
         if exc.errno != errno.EISDIR:
             raise
 
+# loads json files into an array and displays content
 template_array = []
 with open('template.json') as my_file:
         for line in my_file:
@@ -34,6 +35,15 @@ with open('template.json') as f:
 
 pprint(template)
 
+def updateTemplate():
+        template = open("template.json", "r") # opens JSON template file for reading
+        data = json.load(template) # reads template into buffer
+        template.close() # closes template file
+
+        rd1_tmp = data["input_fastq_read1_files"]
+        rd2_tmp = data["input_fastq_read2_files"]
+        data["input_fastq_read1_files"] = rd1_path
+        data["input_fastq_read2_files"] = rd2_path
 
 # read_path = '/data/scratch/rna-seq/RNASeq_Dec2018/*.fastq'
 # read_files = glob.glob(read_path)
