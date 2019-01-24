00f4829b96a8f7808ba29bf56b2ad2dfb9f03f64
Nathaniel <njmaki@mtu.edu>
2019-01-24 11:37:46 -0500

    Update ml_classification.py

--- rna-seq/json_generator/ml_classification.py
+++ rna-seq/json_generator/ml_classification.py
@@ -3,4 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-design_file = pd.read_table('/data/projects/Biocore/biocore-pipelines/rna-seq/json_generator/Sample_DF_JR08-18.txt')
+design_file = pd.read_table('Sample_DF_JR08-18.txt')
+
+print(design_file.head())
